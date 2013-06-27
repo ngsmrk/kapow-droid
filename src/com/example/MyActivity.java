45cbfe89234d18943ab009907abfdea1d46033a1
Angus Mark <ngsmrk@gmail.com>
2013-06-27 08:31:32 +0100

    Added onclick to button

--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -2,14 +2,24 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MyActivity extends Activity
 {
+    private static final String TAG = MyActivity.class.getName();
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Log.d(TAG, "Created");
+    }
+
+    public void sendMessage(View view) {
+        Log.d(TAG, "Sending message");
     }
 }
