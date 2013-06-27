edca93982706bd0a6a243d8e0aaf80a21b429f2d
Angus Mark <ngsmrk@gmail.com>
2013-06-27 08:45:56 +0100

    added activity to display text entered

--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -1,18 +1,22 @@
 package com.example;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
-public class MyActivity extends Activity
-{
+public class MyActivity extends Activity {
     private static final String TAG = MyActivity.class.getName();
 
-    /** Called when the activity is first created. */
+    public final static String EXTRA_MESSAGE = new StringBuilder(MyActivity.class.getPackage().getName()).append("EXTRA_MESSAGE").toString();
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
@@ -21,5 +25,11 @@ public class MyActivity extends Activity
 
     public void sendMessage(View view) {
         Log.d(TAG, "Sending message");
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
