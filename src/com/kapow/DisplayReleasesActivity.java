695d800ed6e5aee6a725de165f54b9c0410868ef
Angus Mark <ngsmrk@gmail.com>
2013-06-28 22:29:38 +0100

    Working towards listview for releases.

--- src/com/kapow/DisplayReleasesActivity.java
+++ src/com/kapow/DisplayReleasesActivity.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DisplayReleasesActivity extends Activity {
     private static final String TAG = DisplayReleasesActivity.class.getName();
@@ -19,28 +21,29 @@ public class DisplayReleasesActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = getIntent();
-        TextView textView = new TextView(this);
-        textView.setVerticalScrollBarEnabled(true);
-        textView.setTextSize(40);
+        final Intent intent = getIntent();
+        final String resultString = intent.getStringExtra(RELEASE_DATA);
 
-        String resultString = intent.getStringExtra(RELEASE_DATA);
-
+        List<String> titles = new ArrayList<String>();
         String shippingDate = "";
         try {
             JSONObject jsonData = new JSONObject(resultString);
             shippingDate = (String) jsonData.get("shipping_date");
-            final JSONArray jsonArray = jsonData.names();
+            final JSONArray jsonArray = jsonData.getJSONArray("comix");
             for (int i = 0; i < jsonArray.length(); i++) {
-                Log.d(TAG, jsonArray.get(i).toString());
+                final String title = ((JSONObject) jsonArray.get(i)).get("title").toString();
+                Log.d(TAG, title);
+                titles.add(title);
             }
         } catch (JSONException e) {
             Log.e(TAG, "Failed to parse response", e);
         }
 
-        textView.setText(resultString);
+        String[] array = titles.toArray((new String[0]));
+        Log.d(TAG, new Integer(array.length).toString());
 
-        setContentView(textView);
+        setContentView(R.layout.release_list_view);
+
         setTitle(intent.getStringExtra(TITLE).concat(" : ").concat(shippingDate));
     }
 }
