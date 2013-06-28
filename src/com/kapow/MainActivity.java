6d5c65f663cf4289629ee5e85e375a6278c9fb46
Angus Mark <ngsmrk@gmail.com>
2013-06-28 21:24:19 +0100

    Optimised imports.

--- src/com/kapow/MainActivity.java
+++ src/com/kapow/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
